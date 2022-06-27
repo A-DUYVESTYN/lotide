f4a8e782d1cf95cd2e47e76652646059cd1037f0
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-06-27 22:03:35 +0000

    syntax improved

--- tail.js
+++ tail.js
@@ -1,7 +1,7 @@
-// returns arraw with first element removed
+// returns array with first element removed
 const tail = function(arr) {
   return arr.slice(1);
-}
+};
 
 // assert function with pass/fail emoji graphics
 const assertEqual = function(actual, expected) {
@@ -18,11 +18,11 @@ const assertEqual = function(actual, expected) {
 const result = tail([1,2,3]);
 const expected = [2,3];
 for (let i = 0; i < result.length; i++) {
-  assertEqual(result[i],expected[i])
+  assertEqual(result[i],expected[i]);
   
 }
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
