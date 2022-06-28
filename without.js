1d4a57ae138a0e106d503dfe73a23139af159bca
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-06-28 21:04:52 +0000

    syntax improved

--- without.js
+++ without.js
@@ -11,8 +11,8 @@ const eqArrays = function (arr1, arr2) {
   return true
 }
 // tests if an actual array result is equivalent to an expected result. does not work for non-arrays
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual,expected)) {
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
     console.log(`✅️✅️✅️Assertion passed: ${actual} === ${expected}`);
   }
   else {
@@ -26,18 +26,18 @@ const assertArraysEqual = function(actual, expected) {
 // check if each element is included in itemsToRemove array
 // if not included, push the current element to the output array. 
 // return output array
-const without = function(source, itemsToRemove) {
+const without = function (source, itemsToRemove) {
   const outputArr = [];
   for (let i = 0; i < source.length; i++) {
     const currentSourceElement = source[i]
     if (!itemsToRemove.includes(currentSourceElement)) {
       outputArr.push(currentSourceElement);
     }
-    }
-    return outputArr;
   }
+  return outputArr;
+}
 
 //Test cases
-assertArraysEqual(without([1, 2, 3], [1]),[2, 3]) // => [2, 3]
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]),["1", "2"])  // => ["1", "2"]
-assertArraysEqual(without([5, "cat", NaN], [NaN, 5, "3"]),["cat"]) 
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]) // => [2, 3]
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"])  // => ["1", "2"]
+assertArraysEqual(without([5, "cat", NaN], [NaN, 5, "3"]), ["cat"]) 
