6c93b9cbd216702106fc53705c401281ca73ce74
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-06-28 22:53:18 +0000

    added flatten.js

---
+++ flatten.js
@@ -0,0 +1,40 @@
+// function returns true if two arrays are equal
+// the result of checking each element is stored in equalArr
+const eqArrays = function(arr1, arr2) {
+  let equalArr = [];
+  for (const i of arr1) {
+    (arr1[i] === arr2[i]) ? equalArr[i] = true : equalArr[i] = false;
+  }
+  if (equalArr.includes(false)) {
+    return false;
+  }
+  return true;
+};
+
+// tests if an actual array result is equivalent to an expected result. does not work for non-arrays
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅️✅️✅️Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚩️🚩️🚩️Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+//takes in an array containing elements including nested arrays of elements, and return a "flattened" version of the array.
+const flatten = function(arr) {
+  const flatArr = [];
+  for (const i of arr) {
+    if (Array.isArray(i)) {
+      i.forEach(element => {
+        flatArr.push(element);
+      });
+    } else {
+      flatArr.push(i);
+    }
+  }
+  return flatArr;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
