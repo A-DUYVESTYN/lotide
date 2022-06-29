efe4f1919a32c68a9cbed3c702d950a349bbe423
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-06-29 18:25:05 +0000

    initial commit of countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+// tests if an actual result is equivalent to an expected result
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️Assertion passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`🚩️🚩️🚩️Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+// counts letters in an input string and outputs an object of counts. ignores whitespace.
+const countLetters = function (str) {
+  const noSpaces = str.split(' ').join('')
+  const outputObj = {}
+  for (const i of noSpaces) {
+    !outputObj[i] ? outputObj[i] = 1 : outputObj[i] ++
+  }
+  return outputObj
+}
+
+console.log(countLetters("lighthouse in the house"))
