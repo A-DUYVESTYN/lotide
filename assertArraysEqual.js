9a0960dfc4e09c477ac727bb8a8a1bf80f14e5c8
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-07-04 19:06:26 +0000

    require eqArrays and export module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,4 @@
-// function returns true if two arrays are equal. Can handle nested arrays using recursion
-// the result of checking each element is stored in equalArr
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  }
-  for (let i = 0; i < arr1.length; i++) {
-
-    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
-      if (!eqArrays(arr1[i], arr2[i])) {
-        return false
-      }
-    } else if (arr1[i] !== arr2[i]) {
-      return false
-    }
-  }
-  return true
-};
+const eqArrays = require('./eqArrays')
 
 // tests if an actual array result is equivalent to an expected result. does not work for non-arrays
 const assertArraysEqual = function(actual, expected) {
@@ -25,6 +8,9 @@ const assertArraysEqual = function(actual, expected) {
     console.log("🚩️🚩️🚩️Assertion failed: ", actual," !== ", expected);
   }
 };
+
+module.exports = assertArraysEqual;
+
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, "3"]);
